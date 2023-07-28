68a2601c5ba17b2d3e7cbc85c0ba7289dcf32866
gouravmawari <65681877+gouravmawari@users.noreply.github.com>
2023-07-28 08:24:53 +0530

    Add files via upload

---
+++ main..js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+    const { state } = req.body;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=8fe7d938df437fea44d6093f46488387&units=metric`;
+    
+    const request = https.get(url, (apiResponse) => {
+        let data = '';
+        
+        apiResponse.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        apiResponse.on('end', () => {
+            const parsedData = JSON.parse(data);
+            console.log(parsedData.main);
+            res.json(parsedData.main); // Send response back to client
+        });
+    });
+
+    request.on('error', (error) => {
+        console.error(`Error: ${error.message}`);
+        res.status(500).json({ error: 'Error fetching data' });
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
